67c12206b581bfffa6d711c646b03bc6114c6f5d
jack-avery <47289484+jack-avery@users.noreply.github.com>
2024-10-26 04:27:36 -0400

    update flake.nix: use nixos-24.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     naersk.url = "github:nix-community/naersk";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
   };
 
   outputs = { self, flake-utils, naersk, nixpkgs }:
@@ -18,7 +18,7 @@
         };
 
         devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ rustc cargo ];
+          nativeBuildInputs = with pkgs; [ rustc cargo rust-analyzer ];
         };
       }
     );
